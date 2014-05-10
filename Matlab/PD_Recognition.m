a960565af6bb81cddfbfaa25d94b5519466f5130
TheSmiff <tjs1g10@ecs.soton.ac.uk>
2014-05-10 22:57:16 +0100

    Managed to get pattern recognition to work

--- Matlab/PD_Recognition.m
+++ Matlab/PD_Recognition.m
@@ -20,25 +20,29 @@ hlogo = axes('Units','Pixels','Position',[440,340,200,185]);
 imshow('uoslogogrey.jpg');
 %Open Button
 hopen = uicontrol('Style','pushbutton','String','Open File',...
-    'Position',[425,350,75,25],'BackgroundColor', [0.9 0.9 0.9],...
+    'Position',[425,350,100,25],'BackgroundColor', [0.9 0.9 0.9],...
     'Callback',{@openbtn_Callback});
 %Run Button
 hrun  = uicontrol('Style','pushbutton','String','Run PSA',...
-    'Position',[425,300,75,25],'BackgroundColor', [0.9 0.9 0.9],...
+    'Position',[425,300,100,25],'BackgroundColor', [0.9 0.9 0.9],...
     'Callback',{@runbtn_Callback});
 
 %Run Recognition Button
 hrec  = uicontrol('Style','pushbutton','String','Run Recognition',...
     'Position',[425,50,100,25],'BackgroundColor', [0.9 0.9 0.9],...
     'Callback',{@recbtn_Callback});
+hrectext = uicontrol('Style','text','String','',...
+    'HorizontalAlignment', 'left',...
+    'BackgroundColor', [0.8 0.8 0.8],...
+    'Position',[535,45,130,25]);
 %File text
 hfile = uicontrol('Style','text','String','Select a File to Proceed',...
     'HorizontalAlignment', 'left',...
     'BackgroundColor', [0.8 0.8 0.8],...
-    'Position',[510,345,130,25]);
+    'Position',[535,345,130,25]);
 %Drop Down Menu
 hpopup = uicontrol('Style','popupmenu',...
-    'String',{'Delta U','Delta T','Delta U & Delta T','Voltage - Time','Voltage - Time 1 cycle'},...
+    'String',{'Delta U','Delta T','Delta U & Delta T','Voltage - Time','Voltage - Time 1 cycle','Classification by dU/dT'},...
     'Position',[425,250,200,25],...
     'Callback',{@popup_menu_Callback});
 %Axes
@@ -100,6 +104,14 @@ SurfaceName = get(hsurf, 'String');
 SurfacePath = 0;
 VoidName = get(hvoid, 'String');
 VoidPath = 0;
+CUw = 0;
+SUw = 0;
+VUw = 0;
+dUw = 0;
+CUe = 0;
+VUe = 0;
+SUe = 0;
+Class = 0;
 
 % --- Executes on button press in hopen.
     function openbtn_Callback(source,eventdata)
@@ -244,6 +256,19 @@ end
          v = axis;
          axis([0.02, 0.04, -1, 1]);
          hold off;
+
+     case 'Classification by dU/dT' % User selects Peaks.
+         scatter(dUw(:,1),dUw(:,2), '.b');
+         hold on
+         scatter(CUw(:,1),CUw(:,2), '.r');
+         scatter(SUw(:,1),SUw(:,2), '.g');
+         scatter(VUw(:,1),VUw(:,2), '.m');
+         hold off
+         title('Filtered Data - Voltage Time Graph');
+         xlabel('Time (s)');
+         ylabel('Voltage (mV)'); 
+         legend('\DeltaU plot Features', 'Corona Features', 'Surface Features', 'Void Features');
+         hold off;
         
       end
    end
@@ -268,54 +293,80 @@ end
                     else
                         clear dU dT dUm1 dTm1 Div Div1 T I If n U 
                         clear C.dU C.dUm1 C.dT C.dTm1 C.Div C.Div1
-                        clear V.dU V.dUm1 V.dT V.dTm1 V.Div V.Div1S.dU
+                        clear V.dU V.dUm1 V.dT V.dTm1 V.Div V.Div1 S.dU
                         clear S.dUm1 S.dT S.dTm1 S.Div S.Div1
-                        clear CdU SdU VdU CdT SdT SdV maxU dUpad dTpad
-                        [C.dU, ~, C.dT, ~, ~, ~, ~, ~] = PSA(strcat(CoronaPath, CoronaName));
-                        [V.dU, ~, V.dT, ~, ~, ~, ~, ~] = PSA(strcat(SurfacePath, SurfaceName));
-                        [S.dU, ~, S.dT, ~, ~, ~, ~, ~] = PSA(strcat(VoidPath, VoidName));
+                        clear CdU SdU VdU CdT SdT SdV maxU dUpad dTpad Class
+                        [C.dU, C.dUm1, C.dT, C.dTm1, C.Div, C.Div1, ~, ~] = PSA(strcat(CoronaPath, CoronaName));
+                        [V.dU, V.dUm1, V.dT, V.dTm1, S.Div, S.Div1, ~, ~] = PSA(strcat(SurfacePath, SurfaceName));
+                        [S.dU, S.dUm1, S.dT, S.dTm1, V.Div, V.Div1, ~, ~] = PSA(strcat(VoidPath, VoidName));
                         [dU, dUm1, dT, dTm1, Div, Div1, T, U]= PSA(FullPathName);
-                                                
-                        %Pad dU and dT with 0s to maximum length
-                        maxU = length(C.dU);
-                        if(maxU < length(V.dU))
-                            maxU = length(V.dU);
-                        end
-                        if(maxU < length(S.dU))
-                            maxU = length(S.dU);
-                        end
-                        if(maxU < length(dU))
-                            maxU = length(dU);
-                        end
-
-                        dUpad = [dU; transpose(zeros(1,(maxU-length(dU))))];
-                        dTpad = [dT; transpose(zeros(1,(maxU-length(dT))))];
-                        C.dU = [C.dU; transpose(zeros(1,(maxU-length(C.dU))))];
-                        C.dT = [C.dT; transpose(zeros(1,(maxU-length(C.dT))))];
-                        S.dU = [S.dU; transpose(zeros(1,(maxU-length(S.dU))))];
-                        S.dT = [S.dT; transpose(zeros(1,(maxU-length(S.dT))))];
-                        V.dU = [V.dU; transpose(zeros(1,(maxU-length(V.dU))))];
-                        V.dT = [V.dT; transpose(zeros(1,(maxU-length(V.dT))))];
-
                         
-                        UC = diag(pdist2(dUpad, C.dU));
-                        US = diag(pdist2(dUpad, S.dU));
-                        UV = diag(pdist2(dUpad, V.dU));
-                        TC = diag(pdist2(dTpad, C.dT));
-                        TS = diag(pdist2(dTpad, S.dT));
-                        TV = diag(pdist2(dTpad, V.dT));
-
-                        assignin('base', 'UC', UC);
-                        assignin('base', 'US', US);
-                        assignin('base', 'UV', UV);
-                        assignin('base', 'TC', TC);
-                        assignin('base', 'TS', TS);
-                        assignin('base', 'TV', TV);
-                        assignin('base', 'dUpad', dUpad);
-                        assignin('base', 'dTpad', dTpad);
-                        assignin('base', 'maxU', maxU);
-
+                        CUmat = [transpose(C.Div); transpose(C.Div1)];
+                        SUmat = [transpose(S.Div); transpose(S.Div1)];
+                        VUmat = [transpose(V.Div); transpose(V.Div1)];
+                        dUmat = [transpose(Div); transpose(Div1)];
                         
+                        CUnet = competlayer(20,.1);
+                        CUnet = configure(CUnet,CUmat);
+                        CUnet.trainParam.epochs = 15;
+                        CUnet = train(CUnet,CUmat);
+                        CUw = CUnet.IW{1};
+                        
+                        SUnet = competlayer(20,.1);
+                        SUnet = configure(SUnet,SUmat);
+                        SUnet.trainParam.epochs = 15;
+                        SUnet = train(SUnet,SUmat);
+                        SUw = SUnet.IW{1};
+                        
+                        VUnet = competlayer(20,.1);
+                        VUnet = configure(VUnet,VUmat);
+                        VUnet.trainParam.epochs = 15;
+                        VUnet = train(VUnet,VUmat);
+                        VUw = VUnet.IW{1};
+                        
+                        dUnet = competlayer(20,.1);
+                        dUnet = configure(dUnet,dUmat);
+                        dUnet.trainParam.epochs = 15;
+                        dUnet = train(dUnet,dUmat);
+                        dUw = dUnet.IW{1};
+                        
+                        CUe = mean(pdist2(dUw, CUw, 'euclidean','Smallest', 1));
+                        SUe = mean(pdist2(dUw, SUw, 'euclidean','Smallest', 1));
+                        VUe = mean(pdist2(dUw, VUw, 'euclidean','Smallest', 1));
+                        
+                        Sum = CUe + SUe + VUe;
+                        CUe = Sum - CUe;
+                        SUe = Sum - SUe;
+                        VUe = Sum - VUe;
+                        Sum = CUe + SUe + VUe;
+                        CUe = CUe*(100/Sum);
+                        SUe = SUe*(100/Sum);
+                        VUe = VUe*(100/Sum);
+                        
+                        Class = '';
+                        if((CUe > SUe) && (CUe > VUe))
+                            Class = ['Corona: ', num2str(CUe, '%2.1f'), '%'];
+                        end
+                        if((SUe > CUe) && (SUe > VUe))
+                            Class = ['Surface: ', num2str(SUe, '%2.1f'), '%'];
+                        end
+                        if((VUe > CUe) && (VUe > SUe))
+                            Class = ['Void: ', num2str(VUe, '%2.1f'), '%'];
+                        end
+                        set(hrectext, 'String', Class);
+                        
+                        assignin('base', 'CUmat', CUmat);
+                        assignin('base', 'SUmat', SUmat);
+                        assignin('base', 'VUmat', VUmat);
+                        assignin('base', 'dUmat', dUmat);
+                        assignin('base', 'CUw', CUw);
+                        assignin('base', 'SUw', SUw);
+                        assignin('base', 'VUw', VUw);
+                        assignin('base', 'dUw', dUw);
+                        assignin('base', 'CUe', CUe);
+                        assignin('base', 'SUe', SUe);
+                        assignin('base', 'VUe', VUe);
+                                               
                         for i = 1:length(dU)
                             assignin('base', 'dU', dU);
                             assignin('base', 'dT', dT);
